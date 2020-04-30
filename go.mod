aacfe94f1803e1dbe527f9a02595709834ada8b1
Daniel Pacak <pacak.daniel@gmail.com>
2020-04-30 13:26:25 +0200

    feat: Add POST /redis/key endpoint to test write access (#3)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/FZambia/sentinel v1.1.0
 	github.com/caarlos0/env/v6 v6.2.2
 	github.com/gomodule/redigo v1.7.0
+	github.com/gorilla/mux v1.7.4
 	github.com/sirupsen/logrus v1.5.0
 )
--- pkg/http/api/handler.go
+++ pkg/http/api/handler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/danielpacak/redis-sentinel-client-go-seed/pkg/persistence"
-
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -17,14 +17,45 @@ func NewHandler(store persistence.Store) http.Handler {
 	handler := &handler{
 		store: store,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.getKeys)
-	return mux
 
+	router := mux.NewRouter()
+	router.Use(handler.logRequest)
+
+	redisRouter := router.PathPrefix("/redis").Subrouter()
+	redisRouter.Methods(http.MethodPost).Path("/key").HandlerFunc(handler.setKey)
+	redisRouter.Methods(http.MethodGet).Path("/keys").HandlerFunc(handler.getKeys)
+
+	return router
 }
 
-func (h *handler) getKeys(w http.ResponseWriter, r *http.Request) {
-	log.Debug("Handling GET /keys request")
+func (h *handler) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Tracef("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (h *handler) setKey(w http.ResponseWriter, r *http.Request) {
+	jsonRequest := struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&jsonRequest)
+	if err != nil {
+		log.WithError(err).Error("Error while decoding JSON request")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = h.store.Set(jsonRequest.Key, jsonRequest.Value)
+	if err != nil {
+		log.WithError(err).Error("Error while setting key")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (h *handler) getKeys(w http.ResponseWriter, _ *http.Request) {
 	keys, err := h.store.Keys()
 	if err != nil {
 		log.WithError(err).Error("Error while getting keys")
